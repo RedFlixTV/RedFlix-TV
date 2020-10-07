e940868617b5cdec6c8bec429ba0a939308fc260
RedFlixTV <53303735+RedFlixTV@users.noreply.github.com>
2020-10-07 21:09:07 +0530

    Add files via upload

---
+++ edu/ptvcric.php
@@ -0,0 +1,3 @@
+<?php
+header("Location: https://prod-fastly-eu-central-1.video.pscp.tv/Transcoding/v1/hls/BKN-vNTUmofIsmNL0lxbStcKzCjk8Eykpm7mxTrj3lOPDnfVosMlZmrUTr_RONC2-UrlqJi2JwgMEtdsfM-k5Q/non_transcode/eu-central-1/periscope-replay-direct-prod-eu-central-1-public/ps_dynamic_hlsproducer.m3u8?type=live");
+?>
